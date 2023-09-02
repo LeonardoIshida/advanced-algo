4c77208df785155749a614b141cf34c59c420853
Leonardo Ishida <leoishida@hotmail.com>
2023-09-02 02:25:01 -0300

    update

---
+++ cpp/06/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int> estatuas, int num_est) {
+    vector<int> range(1e9);
+    memset(&range, 1, 0);
+}
+
+int main() {
+    vector<int> estatuas;
+    int num_estatuas;
+    int num_expl;
+    int tmp;
+
+    for (int i = 0; i < num_estatuas; i++) {
+        cin >> tmp;
+        estatuas.push_back(tmp);
+    }
+
+    return 0;
+}
