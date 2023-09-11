d16cb069ec1b0b3245b671ad341265c69ec946d0
LeonardoIshida <leoishida@hotmail.com>
2023-09-11 20:33:02 -0300

    tentativa grafos

--- cpp/07/main.cpp
+++ cpp/07/main.cpp
@@ -1,12 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void dijkstra(vector<vector<pair<int, int>>> lista_adj, int origem) {
-    
-}
+struct aresta {
+    int origem;
+    int dest;
+    int peso;
+};
 
-void solve() {
+int solve(vector<aresta> arestas, int num_v, int num_a) {
+    vector<int> passou(num_v+1, 0);
+    int cont_a = 0;
+    int peso_total = 0;
 
+    for (int i = 0; i < num_a; i++) {
+        if (passou[arestas[i].dest] == 0) {
+            passou[arestas[i].dest] = 1;
+            // passou[arestas[i].origem] = 1;
+            cont_a++;
+            peso_total += arestas[i].peso;
+            cout << "total: " << peso_total << endl;
+        }
+
+        if (cont_a == num_v-1) return peso_total;
+    }
+
+    return -1;
 }
 
 int main() {
@@ -14,15 +32,23 @@ int main() {
     int num_a;
     cin >> num_v >> num_a;
 
-    vector<vector<pair<int, int>>> lista_adj(num_v);
-    int peso, tmp_v1, tmp_v2;
+    int orig, dest, peso;
+    vector<aresta> arestas;
     for (int i = 0; i < num_a; i++) {
-        cin >> tmp_v1 >> tmp_v2 >> peso;
+        cin >> orig >> dest >> peso;
 
-        lista_adj[tmp_v1].push_back(make_pair(tmp_v2, peso));
-        lista_adj[tmp_v2].push_back(make_pair(tmp_v1, peso));
+        arestas.push_back({orig, dest, peso});
     }
 
+    sort(arestas.begin(), arestas.end(), [](aresta &a1, aresta &a2) {
+        return a1.peso < a2.peso;
+    });
+
+    for (aresta a : arestas) {
+        cout << "origem: " << a.origem << "dest: " << a.dest << "peso: " << a.peso << endl;
+    }
+
+    cout << solve(arestas, num_v, num_a) << endl;
 
     return 0;
 }
