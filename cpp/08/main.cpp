d78ac486c35b19194c61eb3ef1d4ccc21217a29f
Leonardo Ishida <leoishida@hotmail.com>
2023-09-25 19:59:55 -0300

    final update

--- cpp/08/main.cpp
+++ cpp/08/main.cpp
@@ -3,30 +3,57 @@ using namespace std;
 
 vector<vector<int>> lista_adj;
 vector<int> visitado;
-vector<int> tempo;
-queue<int> pilha;
+vector<int> low;
+stack<int> pilha;
+vector<int> na_pilha;
 int num_v;
 int num_a;
+int num_cfc = 0;
+int tempo_atual = 1;
 
-void tarjan(int tempo_atual, int vert) {
-    tempo[vert] = tempo_atual;
-    visitado[vert] = 1;
+void tarjan(int vert) {
+    low[vert] = tempo_atual;
+    visitado[vert] = tempo_atual;
     pilha.push(vert);
+    na_pilha[vert] = 1;
+    tempo_atual += 1;
+
+    cout << "estou em " << vert << endl;
 
     for (int visinho : lista_adj[vert]) {
-        if (visitado[visinho] == 0)
-            tarjan(tempo_atual+1, visinho);
+        if (visitado[visinho] == 0) {
+            // cout << "indo para " << visinho << endl;
+            tarjan(visinho);
+
+            low[vert] = min(low[vert], low[visinho]);
+        }
+        else if (na_pilha[visinho] == 1) {
+            low[vert] = min(low[vert], visitado[visinho]);
+        }
+    }
+
+    cout << "low[u] = " << low[vert] << " == visitado[vert] = " << visitado[vert] << endl;
+    if (low[vert] == visitado[vert]) {
+        while (pilha.top() != vert) {
+            int v = pilha.top();
+            na_pilha[v] = 0;
+            pilha.pop();
+        }
+        int v = pilha.top();
+        na_pilha[v] = 0;
+        pilha.pop();
+        num_cfc += 1;
     }
 }
 
 void solve() {
     visitado.assign(num_v+1, 0);
-    tempo.assign(num_v+1, -1);
-    int tempo_atual = 0;
+    low.assign(num_v+1, -1);
+    na_pilha.assign(num_v+1, 0);
 
-    for (int i = 0; i < num_v+1; i++) {
+    for (int i = 1; i < num_v+1; i++) {
         if (visitado[i] == 0)
-            tarjan();
+            tarjan(i);
     }
 }
 
@@ -40,7 +67,8 @@ int main() {
 
         lista_adj[orig].push_back(dest);
     }
+    solve();
 
-
+    cout << "num cfc " << num_cfc << endl;
     return 0;
 }
